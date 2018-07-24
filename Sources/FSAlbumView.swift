404201db17f9b0464f550428a15a072c1ec4405d
Taufik Obet <mailtaufik@gmail.com>
2018-07-24 21:17:34 +0700

    Reload FSAlbumView if asset collection changed

--- Sources/FSAlbumView.swift
+++ Sources/FSAlbumView.swift
@@ -340,36 +340,11 @@ final class FSAlbumView: UIView, UICollectionViewDataSource, UICollectionViewDel
     func photoLibraryDidChange(_ changeInstance: PHChange) {
         
         DispatchQueue.main.async {
-            
-            let collectionChanges = changeInstance.changeDetails(for: self.images)
-            if collectionChanges != nil {
+            if let collectionChanges = changeInstance.changeDetails(for: self.images), let collectionView = self.collectionView {
                 
-                self.images = collectionChanges!.fetchResultAfterChanges
-                
-                let collectionView = self.collectionView!
-                
-                if !collectionChanges!.hasIncrementalChanges || collectionChanges!.hasMoves {
-                    
-                    collectionView.reloadData()
-                    
-                } else {
-                    
-                    collectionView.performBatchUpdates({
-                        let removedIndexes = collectionChanges!.removedIndexes
-                        if (removedIndexes?.count ?? 0) != 0 {
-                            collectionView.deleteItems(at: removedIndexes!.aapl_indexPathsFromIndexesWithSection(0))
-                        }
-                        let insertedIndexes = collectionChanges!.insertedIndexes
-                        if (insertedIndexes?.count ?? 0) != 0 {
-                            collectionView.insertItems(at: insertedIndexes!.aapl_indexPathsFromIndexesWithSection(0))
-                        }
-                        let changedIndexes = collectionChanges!.changedIndexes
-                        if (changedIndexes?.count ?? 0) != 0 {
-                            collectionView.reloadItems(at: changedIndexes!.aapl_indexPathsFromIndexesWithSection(0))
-                        }
-                    }, completion: nil)
-                }
-                
+                self.images = collectionChanges.fetchResultAfterChanges
+                collectionView.reloadData()
+
                 self.resetCachedAssets()
             }
         }
